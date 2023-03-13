01004c66a136201f753bb43c73d34fb51d7ecd89
surekadharan <124970063+surekadharan@users.noreply.github.com>
2023-03-13 12:16:51 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,15 @@
+function foo()
+{
+    var res=document.getElementById("name").value;
+    console.log(res);
+    var res1=document.getElementById("name1").value;
+    console.log(res1);
+    var res2=document.getElementById("name2").value;
+    console.log(res2);
+    var res3=document.getElementById("main").value;
+    console.log(res3);
+    var res4=document.getElementById("pass").value;
+    console.log(res4);
+    
+    
+}
